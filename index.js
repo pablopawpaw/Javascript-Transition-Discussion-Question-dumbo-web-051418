b0cf388459a9e31216e8bea79c5b5f652750b22d
pablopawpaw <lpault29@gmail.com>
2018-06-28 10:16:31 -0400

    finished

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+
+let lincolnButton = document.querySelector('#save_lincoln')
+
+lincolnButton.addEventListener('click', (e) => {
+  const intervalInput = document.getElementById('interval').value
+  if(intervalInput) {
+    const zombiePic = document.getElementById('foreground')
+    fadeOutAndCallback(zombiePic, intervalInput);
   }
-  $('#foreground').fadeOut(interval);
+})
+
+function fadeOutAndCallback(image, interval){
+    var opacity = 1;
+    var timer = setInterval(function(){
+        if(opacity < 0.1){
+            clearInterval(timer);
+        }
+        image.style.opacity = opacity;
+        opacity -=  0.1;
+    }, interval/10);
 }
